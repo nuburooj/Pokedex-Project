05181a854b4bbc91080e010105d851e9eae28709
Nuburooj Khattak <nuburoojkhattak@gmail.com>
2023-12-27 19:56:12 -0500

    Populated the Pokemon Details to respective HTML placeholders

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
-// fetch("https://pokeapi.co/api/v2/pokemon/1?limit=1302")
-// .then((resp) => resp.json())
-// .then((data) => renderPokemon(data))
 
-fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
+fetch("https://pokeapi.co/api/v2/pokemon/1")
 .then((resp) => resp.json())
-.then((data) => console.log(data.species))
+.then((data) => renderPokemon(data))
 
 
 function renderPokemon(pokeArr){
     
+    //Locations of Divs
     const PokemonListLocation = document.querySelector('#pokemon-list-location')
     const PokeImage = document.querySelector('.pokemon-picture')
     const PokemonDetailsLocation = document.querySelector('#pokemon-detail-location')
+    
+    //Details Info
+    const PokemonName = document.querySelector('.name')
+    const PokemonType = document.querySelector('.type')
+    const PokemonWeight = document.querySelector('.weight')
+    const PokemonAbilities = document.querySelector('.abilities')
+    const PokemonDescription = document.querySelector('.description')
+    
+    
+    
+    
+    //Making and appending list of pokemon 
+    const PokeName = document.createElement("li")
+    PokeName.textContent = pokeArr.name
+    PokemonListLocation.append(PokeName)
+    
+    
+    //Changing attributes of Pokemon Picture
+    PokeImage.src = pokeArr.sprites.front_default
+    PokeImage.alt = pokeArr.name
+    
+    PokemonType.textContent = pokeArr.types[0,1].type.name
+    PokemonWeight.textContent = pokeArr.weight + " lbs"
+    PokemonAbilities.textContent = pokeArr.abilities[0].ability.name
+    
+    
+    fetch("https://pokeapi.co/api/v2/pokemon-species/1")
+    .then((resp) => resp.json())
+    .then((data) => renderPokemonDetails(data))
 
-    pokeArr.forEach((pokeObj) => {
-        const PokeName = document.createElement("li")
-        PokeName.textContent = pokeObj.name
-        PokemonListLocation.append(PokeName)
 
+    function renderPokemonDetails(PokeSpeciesArr){ 
         
-        PokeImage.alt = pokeObj.name
-        
-    })
+        //Changing Attributes of Details 
+        PokemonName.textContent = PokeSpeciesArr.names[8].name
+        PokemonDescription.textContent = PokeSpeciesArr["flavor_text_entries"][0]["flavor_text"]
+    }
+    
     //console.log(data)
 }
 
